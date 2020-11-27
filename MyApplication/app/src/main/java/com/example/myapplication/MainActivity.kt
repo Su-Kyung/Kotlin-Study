4cd52a056afc8fcdb0ce427e98682b939364e33f
Su-Kyung <slloph720@naver.com>
2020-11-27 23:58:30 +0900

    Create Android Layout examples

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.scrollview_10)  // activity_main 대신 다른 xml 이름 쓰면 그 화면 띄우기 가능
     }
 }

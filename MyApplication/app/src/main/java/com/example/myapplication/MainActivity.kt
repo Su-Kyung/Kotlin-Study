0e03d0165024a00c377577d465f490ce0ba86d52
Su-Kyung <slloph720@naver.com>
2020-12-08 15:13:51 +0900

    Create Layout Practice
    - add drawable resources (안쓰는 리소스 있음)
    - 해상도 관계없이 이미지 넣기
    - 레이아웃 실습

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.scrollview_10)  // activity_main 대신 다른 xml 이름 쓰면 그 화면 띄우기 가능
+        setContentView(R.layout.practice_12)  // activity_main 대신 다른 xml 이름 쓰면 그 화면 띄우기 가능
     }
 }

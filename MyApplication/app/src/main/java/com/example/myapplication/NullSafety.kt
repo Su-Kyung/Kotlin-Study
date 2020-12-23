e22cfa76a09f8533ee17ecc9c06ba54bb79776c1
Su-Kyung <slloph720@naver.com>
2020-12-23 21:24:05 +0900

    Add Resource, Context study
    - Resource 개념, 실습
    - sdk 버전에 따른 분기 처리 포함
    - Context 개념

--- MyApplication/app/src/main/java/com/example/myapplication/NullSafety.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/NullSafety.kt
@@ -19,26 +19,26 @@ class NullSafety : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_null_safety)
 
-        val number1: Int = 10
-//        val number2 : Int = null -> 오류: Null 이 될 수 없는 타입
-        val number2: Int? = null
-
-        // !! 사용하기
-        //val number5: Int = number2 + 10 // 우항이 null 이 될 수 있으므로 불가능
-        val number5: Int = number2!! + 10   // !!는 사람(개발자)이 보장하는 것이기 때문에 위험하다)
-
-//        val number3 = number1? + number2 -> 불가능한 문법
-        val number3 = number2?.plus(number)
-        //Log.d("number", "number3 : "+number3)
-
-        // 삼항연산자 -> 엘비스 연산자 (?:)
-        // Null safety 를 위한 도구
-        val number4 = number2 ?:10  // number2 가 null 이면 :뒤의 값인 10이 들어간다
-        //Log.d("number", "number4 : " + number4)
-
-        lateCar = Car(10)
-        // 위아래 두 줄을 바꾸면 오류 발생: 초기화 안되어있으니까
-        Log.d("number", "late number : " + lateCar.number)
+//        val number1: Int = 10
+////        val number2 : Int = null -> 오류: Null 이 될 수 없는 타입
+//        val number2: Int? = null
+//
+//        // !! 사용하기
+//        //val number5: Int = number2 + 10 // 우항이 null 이 될 수 있으므로 불가능
+//        val number5: Int = number2!! + 10   // !!는 사람(개발자)이 보장하는 것이기 때문에 위험하다)
+//
+////        val number3 = number1? + number2 -> 불가능한 문법
+//        val number3 = number2?.plus(number)
+//        //Log.d("number", "number3 : "+number3)
+//
+//        // 삼항연산자 -> 엘비스 연산자 (?:)
+//        // Null safety 를 위한 도구
+//        val number4 = number2 ?:10  // number2 가 null 이면 :뒤의 값인 10이 들어간다
+//        //Log.d("number", "number4 : " + number4)
+//
+//        lateCar = Car(10)
+//        // 위아래 두 줄을 바꾸면 오류 발생: 초기화 안되어있으니까
+//        Log.d("number", "late number : " + lateCar.number)
     }
 
     fun plus(a:Int, b:Int?): Int {
